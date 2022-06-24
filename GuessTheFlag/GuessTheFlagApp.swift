dbd12dada83c6a0831d37510331285ef598035a6
Andrew A. Schilt <Software.Intelligence@gmail.com>
2022-06-24 18:15:39 -0400

    Initial Commit

---
+++ GuessTheFlag/GuessTheFlagApp.swift
@@ -0,0 +1,17 @@
+//
+//  GuessTheFlagApp.swift
+//  GuessTheFlag
+//
+//  Created by Andrew A. Schilt on 6/24/22.
+//
+
+import SwiftUI
+
+@main
+struct GuessTheFlagApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
